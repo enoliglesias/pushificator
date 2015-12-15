c2a3aca9ad9029b096d41a5096e78e1ba5a8a30a
Enol Iglesias <enoliglesias@gmail.com>
2015-12-15 22:10:28 +0100

    Add response, anthenticate and mount Device in v1

--- app/api/pushificator/v1.rb
+++ app/api/pushificator/v1.rb
@@ -5,5 +5,38 @@ module Pushificator
     content_type :json, 'application/json; charset=UTF-8'
     prefix :api
     format :json
+
+    before do
+      header['Access-Control-Allow-Origin'] = '*'
+      header['Access-Control-Request-Method'] = '*'
+    end
+
+    helpers do
+      def api_response response
+       case response
+       when Integer
+         status response
+       when String
+         response
+       when Hash
+         response
+       when Net::HTTPResponse
+         "#{response.code}: #{response.message}"
+       else
+         status 400 # Bad request
+       end
+      end
+
+      def authenticate!
+       error!('Unauthorized. Invalid or expired token.', 401) unless current_user
+      end
+
+      def clean_params(params)
+       ActionController::Parameters.new(params)
+      end
+
+    end
+
+    mount Device
   end
 end
