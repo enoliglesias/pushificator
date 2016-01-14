3646282af0a84696468816257c0dd49aaf465de6
Enol Iglesias <enoliglesias@gmail.com>
2016-01-14 22:05:18 +0100

    Remove comments in rails_admin.rb

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -27,13 +27,6 @@ RailsAdmin.config do |config|
      navigation_icon 'icon-cog'
    end
 
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
   config.actions do
     dashboard                     # mandatory
@@ -45,9 +38,5 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
-
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
   end
 end
