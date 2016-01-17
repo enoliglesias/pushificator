1d51228fd70b26e0bf2556fdff20a284ffacdbb6
Enol Iglesias <enoliglesias@gmail.com>
2016-01-18 00:03:04 +0100

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114193425) do
+ActiveRecord::Schema.define(version: 20160117202815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,16 @@ ActiveRecord::Schema.define(version: 20160114193425) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sendings", force: :cascade do |t|
+    t.integer  "notification_id"
+    t.string   "type"
+    t.datetime "sending_at"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "sendings", ["notification_id"], name: "index_sendings_on_notification_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "",    null: false
     t.string   "encrypted_password",     limit: 255, default: "",    null: false
@@ -66,4 +76,5 @@ ActiveRecord::Schema.define(version: 20160114193425) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "sendings", "notifications"
 end
