01033d0af8201ff1b361a5a7672a6ca49c964b3f
Enol Iglesias <enoliglesias@gmail.com>
2016-01-18 00:05:39 +0100

    routes and I18n

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-
-
   devise_for :user
 
+  resources :notifications do
+    get "send", on: :member
+  end
+
   root to: "rails_admin/main#dashboard"
+
 
   mount Pushificator::V1 => '/'
   mount GrapeSwaggerRails::Engine, at: "/documentation"
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
 end
